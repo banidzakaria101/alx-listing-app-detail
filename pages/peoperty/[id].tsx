0d5f6f62ea088da49002919f623b94506d1545d1
zakariabanid101 <banidzakaria2@gmail.com>
2025-06-13 16:09:30 +0100

    uodate [id] file

--- pages/peoperty/[id].tsx
+++ pages/peoperty/[id].tsx
@@ -1,17 +1,17 @@
-import PropertyDetail from "@/components/property/PropertyDetail";
-import { PROPERTYLISTINGSAMPLE } from "@/constants";
+import { PROPERTYLISTINGSAMPLE } from "@/constants/index";
 import { useRouter } from "next/router";
+import PropertyDetail from "@/components/property/PropertyDetail";
 
 export default function PropertyPage() {
-    const router = useRouter();
-    const { id } = router.query;
-    const property = PROPERTYLISTINGSAMPLE.find((item) => ClipboardItem.name === id);
+  const router = useRouter();
+  const { id } = router.query;
+  const property = PROPERTYLISTINGSAMPLE.find((item) => item.name === id);
 
-    if (!property) return <p>Poperty not found</p>;
+  if (!property) return <p>Property not found</p>;
 
-    return (
-        <div>
-            <PropertyDetail property={property} />
-        </div>
-    )
+  return (
+    <div>
+      <PropertyDetail property={property} />
+    </div>
+  );
 }
